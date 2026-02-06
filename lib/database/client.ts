f748688e81499f8c40a62da7481d4f164917baf1
justrawaccel <public@justrawaccel.dev>
2026-02-06 14:15:22 +0200

    feat: add database client and update exports

---
+++ lib/database/client.ts
@@ -0,0 +1,14 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@db/client';
+import { isDevelopment } from '@lib/constants';
+import pg from 'pg';
+
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
+const db = new PrismaClient({
+  adapter,
+  log: isDevelopment ? ['query', 'error', 'warn'] : ['error']
+});
+
+export { db };
--- lib/index.ts
+++ lib/index.ts
@@ -3,3 +3,4 @@
 export * from './logger';
 export * from './constants';
 export * from './config';
+export * from './database';
